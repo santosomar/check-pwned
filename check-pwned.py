c884e80df6be19d6939b238f558607eab50fa2ba
Pedro Sampaio <phcsss@gmail.com>
2017-01-22 01:11:11 -0200

    added cmd args suport

--- check-pwned.py
+++ check-pwned.py
@@ -1,20 +1,57 @@
 import requests
 import json
+import argparse
 from time import sleep
 
-site = "https://haveibeenpwned.com/api/v2/breachedaccount/"
+parser = argparse.ArgumentParser(
+        description='Check for pwned accounts.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument(
+        '-a',
+        '--account',
+        help='An email account or a login name.')
+group.add_argument(
+        '-f',
+        '--file',
+        help='Read accounts from file (one account per line)')
+args = parser.parse_args()
 
-f = open('accounts.txt','rb')
 
-for account in f:
-    email =  account.decode("utf-8").rstrip()
-    url = site + email
-    resp = requests.get(url)
-    if resp.text:
-        t = json.loads(resp.text)
-        print(email + ' - PWNED\n')
-        print(t)
-        print('\n')
-    else:
-        print(email + ' - CLEAN\n')
-    sleep(2)
+def haveibeenpwned(account=None, file=None):
+    site = "https://haveibeenpwned.com/api/v2/breachedaccount/"
+
+    if account:
+        email = account.rstrip()
+        url = site + email
+        resp = requests.get(url)
+        if resp.text:
+            t = json.loads(resp.text)
+            print(email + ' - PWNED\n')
+            print(t)
+            print('\n')
+        else:
+            print(email + ' - CLEAN\n')
+        sleep(2)
+
+    if file:
+        f = open(file, 'rb')
+
+        for a in f:
+            email = a.decode("utf-8").rstrip()
+            url = site + email
+            resp = requests.get(url)
+            if resp.text:
+                t = json.loads(resp.text)
+                print(email + ' - PWNED\n')
+                print(t)
+                print('\n')
+            else:
+                print(email + ' - CLEAN\n')
+            sleep(2)
+
+
+if args.account:
+    haveibeenpwned(account=args.account)
+
+if args.file:
+    haveibeenpwned(file=args.file)
