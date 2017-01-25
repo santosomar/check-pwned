72ec246065baf8c1ac44e5d528baa1f921bd0680
pedro.sampaio <pedro.sampaio@ufba.br>
2017-01-25 15:21:05 -0200

    changed main script to use hibp class

--- check-pwned.py
+++ check-pwned.py
@@ -1,6 +1,5 @@
-import requests
-import json
 import argparse
+import hibp
 from time import sleep
 
 parser = argparse.ArgumentParser(
@@ -23,41 +22,15 @@ parser.add_argument(
 args = parser.parse_args()
 
 
-def haveibeenpwned(account=None, file=None):
-    site = "https://haveibeenpwned.com/api/v2/breachedaccount/"
-
-    if account:
-        email = account.rstrip()
-        url = site + email
-        resp = requests.get(url)
-        if resp.text:
-            t = json.loads(resp.text)
-            print(email + ' - PWNED\n')
-            print(t)
-            print('\n')
-        else:
-            print(email + ' - CLEAN\n')
-
-    if file:
-        f = open(file, 'rb')
-
-        for a in f:
-            email = a.decode("utf-8").rstrip()
-            url = site + email
-            resp = requests.get(url)
-            if resp.text:
-                t = json.loads(resp.text)
-                print(email + ' - PWNED\n')
-                print(t)
-                print('\n')
-            else:
-                print(email + ' - CLEAN\n')
-            sleep(2)
-        f.close()
-
 if args.database == 'hibp' or 'haveibeenpwned':
+    checker = hibp.hibp()
     if args.account:
-        haveibeenpwned(account=args.account)
+        checker.check(args.account.rstrip())
 
     if args.file:
-        haveibeenpwned(file=args.file)
+        f = open(args.file, 'r')
+        for a in f:
+            checker.check(a.rstrip())
+            sleep(2)
+        f.close()
+        del checker
