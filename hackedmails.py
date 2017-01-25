f12f03e05529e0392638f5391f54fa87400ffc08
pedro.sampaio <pedro.sampaio@ufba.br>
2017-01-25 16:08:56 -0200

    created hackedmails database class

---
+++ hackedmails.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+class hm():
+    """Class for hacked-mails.com database API"""
+    def __init__(self):
+        self.site="https://hacked-emails.com/api?q="
+
+    def get_account(self, account=""):
+        """Makes the GET request to the database"""
+        self.name=account
+        self.url = self.site + self.name
+        return requests.get(self.url)
+
+    def check(self, account=""):
+        self.name=account
+        self.response = self.get_account(self.name)
+        self.jresponse = json.loads(self.response.text)
+        if self.jresponse['status'] == 'found':
+            print(self.name +  ' - PWNED\n')
+            print(self.jresponse)
+        else:
+            print(self.name + ' - CLEAN')
+
